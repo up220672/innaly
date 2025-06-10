166cd093a21341d4e357c43d9c0ed575be90343c
UP220672 <up220672@alumnos.upa.edu.mx>
2025-06-10 12:13:45 -0600

    feat(RoomService): initialize RoomService with Express setup and basic API endpoint

---
+++ microservices/APIGatewayService/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Middleware para parsear JSON
+app.use(express.json());
+
+// Ruta de ejemplo
+app.get('/', (req, res) => {
+  res.send('Bienvenido al API Gateway');
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor en http://localhost:${PORT}`);
+});
